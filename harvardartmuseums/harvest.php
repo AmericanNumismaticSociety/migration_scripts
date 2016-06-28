a12342792143b3f5c0817ad6727818078eea6bcf
Ethan Gruber <gruber@numismatics.org>
2016-06-28 12:37:29 -0400

    updated script for RIC 6-10, made it more flexible in finding ids

--- harvardartmuseums/harvest.php
+++ harvardartmuseums/harvest.php
@@ -18,8 +18,7 @@ function parse_page($page, $apiKey){
 	GLOBAL $records;
 	
 	echo "Page {$page}\n";
-	
-	$service = 'http://api.harvardartmuseums.org/object?size=100&classification=Coins&q=department:"Department%20of%20Ancient%20and%20Byzantine%20Art%20%26%20Numismatics"&apikey=' . $apiKey . '&page=' . $page;
+	$service = 'http://api.harvardartmuseums.org/object?size=100&classification=Coins&q=(standardreferencenumber:RRC+OR+standardreferencenumber:RIC+OR+standardreferencenumber:Price)+AND+department:"Department%20of%20Ancient%20and%20Byzantine%20Art%20%26%20Numismatics"&apikey=' . $apiKey . '&page=' . $page;
 	
 	$json = file_get_contents($service);
 	$data = json_decode($json);
@@ -68,87 +67,123 @@ function parse_page($page, $apiKey){
 				$records[] = $row;
 				//end PELLA processing				
 			} elseif (preg_match('/RIC/', $reference)){
-				preg_match('/(RIC[^,]+),\s(.*)/', $reference, $matches);
+				$originalReference = $reference;
 				
-				if (isset($matches[1])){
-					switch ($matches[1]){
-						case 'RIC I 2':
-							$volume = 'ric.1(2)';
-							break;
-						case 'RIC II.1 2':
-							$volume = 'ric.2_1(2)';
-							break;
-						case 'RIC II':
-							$volume = 'ric.2';
-							break;
-						case 'RIC III':
-							$volume = 'ric.3';
-							break;
-						case 'RIC IV':
-						case 'RIC IV(B)':
-						case 'RIC IV(C)':
-							$volume = 'ric.4';
-							break;
-						case 'RIC V':
-						case 'RIC V(1)':
-						case 'RIC V(2)':
-							$volume = 'ric.5';
-							break;
-						default:
-							$volume = null;
+				//1. get volume number and then strip from reference. 2. trim leading or trailing commas. 3. trim spaces
+				if (strpos($reference, 'RIC I 2') !== FALSE){
+					$parsedVolume = 'RIC I 2';					
+					$volume = 'ric.1(2)';					
+				} elseif (strpos($reference, 'RIC II.1 2') !== FALSE){
+					$parsedVolume = 'RIC II.1 2';
+					$volume = 'ric.2_1(2)';										
+				} elseif (strpos($reference, 'RIC II (2)') !== FALSE){
+					$parsedVolume = 'RIC II (2)';
+					$volume = 'ric.2_1(2)';					
+				} elseif (strpos($reference, 'RIC III') !== FALSE){
+					$parsedVolume = 'RIC III';
+					$volume = 'ric.3';					
+				} elseif (strpos($reference, 'RIC II') !== FALSE){
+					$parsedVolume = 'RIC II';
+					$volume = 'ric.2';					
+				} elseif (strpos($reference, 'RIC IV(B)') !== FALSE){
+					$parsedVolume = 'RIC IV(B)';
+					$volume = 'ric.4';					
+				} elseif (strpos($reference, 'RIC IV(C)') !== FALSE){
+					$parsedVolume = 'RIC IV(C)';
+					$volume = 'ric.4';					
+				} elseif (strpos($reference, 'RIC IV') !== FALSE){
+					$parsedVolume = 'RIC IV';
+					$volume = 'ric.4';					
+				}elseif (strpos($reference, 'RIC V(1)') !== FALSE){
+					$parsedVolume = 'RIC V(1)';
+					$volume = 'ric.5';					
+				} elseif (strpos($reference, 'RIC V(2)') !== FALSE){
+					$parsedVolume = 'RIC V(2)';
+					$volume = 'ric.5';
+				} elseif (strpos($reference, 'RIC VIII') !== FALSE){
+					$parsedVolume = 'RIC VIII';
+					$volume = 'ric.8';					
+				} elseif (strpos($reference, 'RIC VII') !== FALSE){
+					$parsedVolume = 'RIC VII';
+					$volume = 'ric.7';					
+				} elseif (strpos($reference, 'RIC VI') !== FALSE){
+					$parsedVolume = 'RIC VI';
+					$volume = 'ric.6';
+				} elseif (strpos($reference, 'RIC V') !== FALSE){
+					$parsedVolume = 'RIC V';
+					$volume = 'ric.5';					
+				} elseif (strpos($reference, 'RIC X') !== FALSE){					
+					$parsedVolume = 'RIC X';
+					$volume = 'ric.10';
+				} else {
+					$volume = null;
+				}
+				
+				echo "{$originalReference}\n";
+					
+				//strip parsed Volume and trailing/leading commas and whitespace
+				$reference = trim(trim(str_replace($parsedVolume, '', $reference), ','));
+				
+				//strip whitespace following the p.
+				$reference = str_replace('p. ', 'p.', $reference);
+
+				//only process currently published volumes
+				if ($volume != null && strlen($reference) > 0){
+					$row = array();
+					
+					//generate record metadata array
+					$row = construct_metadata($record);
+					$row['reference'] = $originalReference;
+					
+					//parse remaining numbers of the reference, determine page number based on 'p.'
+					$arr = explode(' ', $reference);
+					$pageNumber = '';
+					$id = '';
+					foreach ($arr as $item){					
+						if (strpos($item, 'p.') !== FALSE){
+							$pageNumber = trim(trim(str_replace('p.', '', $item), ','));
+						} else {
+							$id = trim(trim($item, ','));
+						}
 					}
-
-					//only process currently published volumes
-					if ($volume != null && isset($matches[2])){
-						$row = array();
 						
-						//generate record metadata array
-						$row = construct_metadata($record);
-						$row['reference'] = $reference;
-						
-						preg_match('/([^\s|^,]+).*?p\.\s?([0-9]+)/', $matches[2], $arr);
-							
-						if (isset($arr[1]) and isset($arr[2])){
-							$id = str_replace(' ', '', $arr[1]);
-							$pageNumber = trim($arr[2]);	
-
-							//ignore uncertain coins
-							if (substr($id, -1) != '?'){
-								$authority = get_authority($matches[1], $pageNumber);
-									
-								if ($authority != null){
-									//if $authority is properly resolved, try OCRE lookups
-									$prefix = "{$volume}.{$authority}.";
-									if (preg_match('/[a-zA-Z]/', $id)){
-										//try uppercase first
-										$upper = strtoupper($id);
-										$cointype = "http://numismatics.org/ocre/id/{$prefix}" . urlencode($upper);
-										$file_headers = @get_headers($cointype);
-										if ($file_headers[0] == 'HTTP/1.1 200 OK'){
-											echo "{$row['objectnumber']}: {$cointype}\n";
-											$row['cointype'] = $cointype;
-										} else {
-											//then try default
-											$cointype = "http://numismatics.org/ocre/id/{$prefix}" . urlencode($id);
-											$file_headers = @get_headers($cointype);
-											if ($file_headers[0] == 'HTTP/1.1 200 OK'){												
-												echo "{$row['objectnumber']}: {$cointype}\n";
-												$row['cointype'] = $cointype;
-											}
-										}
+					if (strlen($id) > 0 && strlen($pageNumber) > 0){
+						//ignore uncertain coins
+						if (substr($id, -1) != '?'){
+							$authority = get_authority($parsedVolume, $pageNumber);
+								
+							if ($authority != null){
+								//if $authority is properly resolved, try OCRE lookups
+								$prefix = "{$volume}.{$authority}.";
+								if (preg_match('/[a-zA-Z]/', $id)){
+									//try uppercase first
+									$upper = strtoupper($id);
+									$cointype = "http://numismatics.org/ocre/id/{$prefix}" . urlencode($upper);
+									$file_headers = @get_headers($cointype);
+									if ($file_headers[0] == 'HTTP/1.1 200 OK'){
+										echo "{$row['objectnumber']}: {$cointype}\n";
+										$row['cointype'] = $cointype;
 									} else {
+										//then try default
 										$cointype = "http://numismatics.org/ocre/id/{$prefix}" . urlencode($id);
 										$file_headers = @get_headers($cointype);
 										if ($file_headers[0] == 'HTTP/1.1 200 OK'){												
 											echo "{$row['objectnumber']}: {$cointype}\n";
 											$row['cointype'] = $cointype;
-										} 
-									}									
-								}								
-							}					
-						}
-						$records[] = $row;
-					}	
+										}
+									}
+								} else {
+									$cointype = "http://numismatics.org/ocre/id/{$prefix}" . urlencode($id);
+									$file_headers = @get_headers($cointype);
+									if ($file_headers[0] == 'HTTP/1.1 200 OK'){												
+										echo "{$row['objectnumber']}: {$cointype}\n";
+										$row['cointype'] = $cointype;
+									} 
+								}									
+							}								
+						}					
+					}
+					$records[] = $row;
 				}				
 				//end RIC processing
 			}
@@ -307,7 +342,7 @@ function get_authority($volume, $p){
 			$authority = 'vit';
 		} 
 		
-	} elseif ($volume == 'RIC II.1 2') {
+	} elseif ($volume == 'RIC II.1 2' || $volume == 'RIC II (2)') {
 		if ($p >= 58 && $p <= 180){
 			$authority = 'ves';
 		} elseif ($p >= 199 && $p <= 236){
@@ -430,6 +465,268 @@ function get_authority($volume, $p){
 	} elseif ($volume == 'RIC V(2)'){
 		if ($p >= 20 && $p <= 121){
 			$authority = 'pro';
+		}
+	} elseif ($volume == 'RIC VI'){
+		if ($p >= 123 && $p <= 140){
+			$authority = 'lon';
+		}
+		elseif ($p >= 163 && $p <= 228){
+			$authority = 'tri';
+		}
+		elseif ($p >= 241 && $p <= 265){
+			$authority = 'lug';
+		}
+		elseif ($p >= 279 && $p <= 298){
+			$authority = 'tic';
+		}
+		elseif ($p >= 310 && $p <= 328){
+			$authority = 'aq';
+		}
+		elseif ($p >= 350 && $p <= 392){
+			$authority = 'rom';
+		}
+		elseif ($p >= 400 && $p <= 410){
+			$authority = 'ost';
+		}
+		elseif ($p >= 422 && $p <= 435){
+			$authority = 'carth';
+		}
+		elseif ($p >= 455 && $p <= 485){
+			$authority = 'sis';
+		}
+		elseif ($p >= 491 && $p <= 500){
+			$authority = 'serd';
+		}
+		elseif ($p >= 509 && $p <= 519){
+			$authority = 'thes';
+		}
+		elseif ($p >= 529 && $p <= 542){
+			$authority = 'her';
+		}
+		elseif ($p >= 553 && $p <= 568){
+			$authority = 'nic';
+		}
+		elseif ($p >= 578 && $p <= 595){
+			$authority = 'cyz';
+		}
+		elseif ($p >= 612 && $p <= 644){
+			$authority = 'anch';
+		}
+		elseif ($p >= 660 && $p <= 686){
+			$authority = 'alex';
+		}
+	} elseif ($volume == 'RIC VII'){
+		if ($p >= 97 && $p <= 116){
+			$authority = 'lon';
+		}
+		elseif ($p >= 122 && $p <= 142){
+			$authority = 'lug';
+		}
+		elseif ($p >= 162 && $p <= 223){
+			$authority = 'tri';
+		}
+		elseif ($p >= 234 && $p <= 279){
+			$authority = 'ar';
+		}
+		elseif ($p >= 296 && $p <= 347){
+			$authority = 'rom';
+		}
+		elseif ($p >= 360 && $p <= 387){
+			$authority = 'tic';
+		}
+		elseif ($p >= 392 && $p <= 147){
+			$authority = 'aq';
+		}
+		elseif ($p >= 422 && $p <= 460){
+			$authority = 'sis';
+		}
+		elseif ($p >= 467 && $p <= 477){
+			$authority = 'sir';
+		}
+		elseif ($p >= 479 && $p <= 480){
+			$authority = 'serd';
+		}
+		elseif ($p >= 498 && $p <= 530){
+			$authority = 'thes';
+		}
+		elseif ($p >= 541 && $p <= 561){
+			$authority = 'her';
+		}
+		elseif ($p >= 569 && $p <= 590){
+			$authority = 'cnp';
+		}
+		elseif ($p >= 597 && $p <= 635){
+			$authority = 'nic';
+		}
+		elseif ($p >= 643 && $p <= 660){
+			$authority = 'cyz';
+		}
+		elseif ($p >= 675 && $p <= 697){
+			$authority = 'anch';
+		}
+		elseif ($p >= 702 && $p <= 712){
+			$authority = 'alex';
+		}
+	} elseif ($volume == 'RIC VIII'){
+		if ($p >= 121 && $p <= 124){
+			$authority = 'amb';
+		}
+		elseif ($p >= 138 && $p <= 169){
+			$authority = 'tri';
+		}
+		elseif ($p >= 177 && $p <= 196){
+			$authority = 'lug';
+		}
+		elseif ($p >= 204 && $p <= 231){
+			$authority = 'ar';
+		}
+		elseif ($p >= 233 && $p <= 233){
+			$authority = 'med';
+		}
+		elseif ($p >= 248 && $p <= 305){
+			$authority = 'rom';
+		}
+		elseif ($p >= 314 && $p <= 338){
+			$authority = 'aq';
+		}
+		elseif ($p >= 348 && $p <= 381){
+			$authority = 'sis';
+		}
+		elseif ($p >= 384 && $p <= 394){
+			$authority = 'sir';
+		}
+		elseif ($p >= 401 && $p <= 425){
+			$authority = 'thes';
+		}
+		elseif ($p >= 429 && $p <= 439){
+			$authority = 'her';
+		}
+		elseif ($p >= 446 && $p <= 465){
+			$authority = 'cnp';
+		}
+		elseif ($p >= 470 && $p <= 485){
+			$authority = 'nic';
+		}
+		elseif ($p >= 489 && $p <= 501){
+			$authority = 'cyz';
+		}
+		elseif ($p >= 511 && $p <= 534){
+			$authority = 'anch';
+		}
+		elseif ($p >= 538 && $p <= 546){
+			$authority = 'alex';
+		}
+	} elseif ($volume == 'RIC X'){
+		if ($p >= 239 && $p <= 252){
+			$authority = 'arc_e';
+		}
+		if ($p >= 253 && $p <= 277){
+			$authority = 'theo_ii_e';
+		}
+		elseif ($p >= 278 && $p <= 283){
+			$authority = 'marc_e';
+		}
+		elseif ($p >= 284 && $p <= 296){
+			$authority = 'leo_i_e';
+		}
+		elseif ($p >= 267 && $p <= 297){
+			$authority = 'leo_ii_e';
+		}
+		elseif ($p >= 297 && $p <= 298){
+			$authority = 'leo_ii-zen_e';
+		}
+		elseif ($p >= 299 && $p <= 300){
+			$authority = 'zeno(1)_e';
+		}
+		elseif ($p >= 301 && $p <= 303){
+			$authority = 'bas_e';
+		}
+		elseif ($p >= 303 && $p <= 305){
+			$authority = 'bas-mar_e';
+		}
+		elseif ($p >= 306 && $p <= 315){
+			$authority = 'zeno(2)_e';
+		}
+		elseif ($p >= 316 && $p <= 316){
+			$authority = 'leon_e';
+		}
+		elseif ($p >= 317 && $p <= 342){
+			$authority = 'hon_w';
+		}
+		elseif ($p >= 343 && $p <= 346){
+			$authority = 'pr_att_w';
+		}
+		elseif ($p >= 347 && $p <= 350){
+			$authority = 'con_iii_w';
+		}
+		elseif ($p >= 351 && $p <= 351){
+			$authority = 'max_barc_w';
+		}
+		elseif ($p >= 352 && $p <= 354){
+			$authority = 'jov_w';
+		}
+		elseif ($p >= 355 && $p <= 358){
+			$authority = 'theo_ii_w';
+		}
+		elseif ($p >= 359 && $p <= 361){
+			$authority = 'joh_w';
+		}
+		elseif ($p >= 363 && $p <= 384){
+			$authority = 'valt_iii_w';
+		}
+		elseif ($p >= 385 && $p <= 385){
+			$authority = 'pet_max_w';
+		}
+		elseif ($p >= 386 && $p <= 387){
+			$authority = 'marc_w';
+		}
+		elseif ($p >= 388 && $p <= 390){
+			$authority = 'av_w';
+		}
+		elseif ($p >= 392 && $p <= 398){
+			$authority = 'leo_i_w';
+		}
+		elseif ($p >= 399 && $p <= 405){
+			$authority = 'maj_w';
+		}
+		elseif ($p >= 406 && $p <= 409){
+			$authority = 'lib_sev_w';
+		}
+		elseif ($p >= 411 && $p <= 421){
+			$authority = 'anth_w';
+		}
+		elseif ($p >= 422 && $p <= 423){
+			$authority = 'oly_w';
+		}
+		elseif ($p >= 424 && $p <= 426){
+			$authority = 'glyc_w';
+		}
+		elseif ($p >= 427 && $p <= 434){
+			$authority = 'jul_nep_w';
+		}
+		elseif ($p >= 435 && $p <= 437){
+			$authority = 'bas_w';
+		}
+		elseif ($p >= 438 && $p <= 441){
+			$authority = 'rom_aug_w';
+		}
+		elseif ($p >= 442 && $p <= 442){
+			$authority = 'odo_w';
+		}
+		elseif ($p >= 443 && $p <= 449){
+			$authority = 'zeno_w';
+		}
+		elseif ($p >= 450 && $p <= 462){
+			$authority = 'visi';
+		}
+		elseif ($p >= 463 && $p <= 464){
+			$authority = 'gallia';
+		}
+		elseif ($p >= 465 && $p <= 466){
+			$authority = 'spa';
+		}
+		elseif ($p >= 467 && $p <= 470){
+			$authority = 'afr';
 		}
 	}
 	
---
+++ harvardartmuseums/process-pages.php
@@ -0,0 +1,59 @@
+<?php 
+
+$data = generate_json('pages.csv');
+
+$text = '';
+$volume = 5;
+foreach ($data as $row){
+	$start = $row['Start Page'];
+	$end = $row['End Page'];	
+	$auth = rtrim($row['Section URI'], '.');
+	
+	$pieces = explode('.', $auth);
+	
+	$text .= ((int)$pieces[1] > $volume ? 'if' : 'elseif') . ' ($p >= ' . $start . ' && $p <= ' . $end . '){' . "\n";
+	$text .= "\t" . '$authority = ' . "'" . $auth . "';\n";
+	$text .= "}\n";
+	
+	if ((int)$pieces[1] > $volume){
+		$volume++;
+	}
+}
+
+file_put_contents('conditionals.txt', $text);
+//echo $text;
+
+function generate_json($doc){
+	$keys = array();
+	$array = array();
+	$csv = csvToArray($doc, ',');
+	// Set number of elements (minus 1 because we shift off the first row)
+	$count = count($csv) - 1;
+	//Use first row for names
+	$labels = array_shift($csv);
+	foreach ($labels as $label) {
+		$keys[] = $label;
+	}
+	// Bring it all together
+	for ($j = 0; $j < $count; $j++) {
+		$d = array_combine($keys, $csv[$j]);
+		$array[$j] = $d;
+	}
+	return $array;
+}
+// Function to convert CSV into associative array
+function csvToArray($file, $delimiter) {
+	if (($handle = fopen($file, 'r')) !== FALSE) {
+		$i = 0;
+		while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
+			for ($j = 0; $j < count($lineArray); $j++) {
+				$arr[$i][$j] = $lineArray[$j];
+			}
+			$i++;
+		}
+		fclose($handle);
+	}
+	return $arr;
+}
+
+?>
