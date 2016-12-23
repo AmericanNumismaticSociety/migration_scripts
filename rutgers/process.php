e1814cc1fdaaed2dbffc25043eaf7837c2405c5f
Ethan Gruber <ewg4xuva@gmail.com>
2016-12-23 17:19:47 -0500

    added PHP script intended to process MODS records in the Rutgers Fedora repository to map RRC coins to CRRO

---
+++ rutgers/process.php
@@ -0,0 +1,203 @@
+<?php 
+$list = array('50413');
+$records = array();
+define("RUPREFIX", 'http://rucore.libraries.rutgers.edu/schemas/rulib/0.1/metadata.dtd');
+
+foreach ($list as $id){
+	process_record($id);
+}
+
+generate_rdf($records);
+
+function process_record($id){
+	GLOBAL $records;
+	
+	
+	$file = "https://rucore.libraries.rutgers.edu/api/get/{$id}/mods/";
+	$dom = new DOMDocument('1.0', 'UTF-8');
+	if ($dom->load($file) === FALSE){
+		echo "{$file} failed to load.\n";
+	} else {
+		$row = array();
+	
+		//load source metadata
+		$sourceFile = "https://rucore.libraries.rutgers.edu/api/get/{$id}/source/1";
+		$sourceXML = new DOMDocument('1.0', 'UTF-8');
+		if ($sourceXML->load($sourceFile) === FALSE){
+			echo "{$sourceFile} failed to load.\n";
+		} else {
+			//process each node in the source XML document
+			foreach ($sourceXML->getElementsByTagNameNS(RUPREFIX, '*') as $element) {
+				$name = $element->nodeName;
+				if ($element->getAttribute('TYPE') == 'Catalog Number'){
+					$row['identifier'] = $element->nodeValue;
+				} else {
+					if ($name == 'rulib:dimensions'){
+						//process diameter
+						preg_match('/(\d+)\smm/', $element->nodeValue, $matches);
+						if (isset($matches[1])){
+							$row['diameter'] = $matches[1];
+						}
+						unset($matches);
+					} elseif ($name == 'rulib:extent'){
+						//process weight
+						preg_match('/(.*)\sg/', $element->nodeValue, $matches);
+						if (isset($matches[1]) && is_numeric($matches[1])){
+							$row['weight'] = $matches[1];
+						}
+						unset($matches);
+					}
+				}
+			}
+		}
+	
+		//process MODS metadata
+		$xpath = new DOMXpath($dom);
+		$xpath->registerNamespace("mods", "http://www.loc.gov/mods/");
+	
+		$ref = $xpath->query("//mods:classification[@authority='Crawford']");
+	
+		if ($ref->length > 0){
+			$row['reference'] = $ref->item(0)->nodeValue;
+			$cointype = 'http://numismatics.org/crro/id/rrc-' . str_replace('/', '.', $ref->item(0)->nodeValue);
+				
+			$file_headers = @get_headers($cointype);
+			if ($file_headers[0] == 'HTTP/1.1 200 OK'){
+				//echo "{$row['objectnumber']}: {$cointype}\n";
+				$row['cointype'] = $cointype;
+			}
+			$row['uri'] = 'http://dx.doi.org/' . $xpath->query('//mods:identifier[@type="doi"]')->item(0)->nodeValue;
+			$row['title'] = $xpath->query('//mods:titleInfo/mods:title')->item(0)->nodeValue;
+		}
+	
+		//images
+		$row['obvThumb'] = "https://rucore.libraries.rutgers.edu/api/iiif/image/2.0/rutgers-lib:{$id};PTIF-1/full/,120/0/default.jpg";
+		$row['revThumb'] = "https://rucore.libraries.rutgers.edu/api/iiif/image/2.0/rutgers-lib:{$id};PTIF-2/full/,120/0/default.jpg";
+		$row['obvRef'] = "https://rucore.libraries.rutgers.edu/api/iiif/image/2.0/rutgers-lib:{$id};PTIF-1/full/600,/0/default.jpg";
+		$row['revRef'] = "https://rucore.libraries.rutgers.edu/api/iiif/image/2.0/rutgers-lib:{$id};PTIF-2/full/600,/0/default.jpg";
+	
+		//var_dump($row);
+	
+		$records[] = $row;
+	}
+}
+
+function generate_rdf($records){
+	//start RDF/XML file
+	//use XML writer to generate RDF
+	$writer = new XMLWriter();
+	//$writer->openURI("rutgers.rdf");
+	$writer->openURI('php://output');
+	$writer->startDocument('1.0','UTF-8');
+	$writer->setIndent(true);
+	//now we need to define our Indent string,which is basically how many blank spaces we want to have for the indent
+	$writer->setIndentString("    ");
+
+	$writer->startElement('rdf:RDF');
+	$writer->writeAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema#');
+	$writer->writeAttribute('xmlns:nm', "http://nomisma.org/id/");
+	$writer->writeAttribute('xmlns:nmo', "http://nomisma.org/ontology#");
+	$writer->writeAttribute('xmlns:dcterms', "http://purl.org/dc/terms/");
+	$writer->writeAttribute('xmlns:foaf', "http://xmlns.com/foaf/0.1/");
+	$writer->writeAttribute('xmlns:geo', "http://www.w3.org/2003/01/geo/wgs84_pos#");
+	$writer->writeAttribute('xmlns:rdf', "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+	$writer->writeAttribute('xmlns:void', "http://rdfs.org/ns/void#");
+
+	foreach ($records as $record){
+		if (isset($record['cointype'])){
+			$writer->startElement('nmo:NumismaticObject');
+			$writer->writeAttribute('rdf:about', $record['uri']);
+			$writer->startElement('dcterms:title');
+			$writer->writeAttribute('xml:lang', 'en');
+			$writer->text($record['title']);
+			$writer->endElement();
+			$writer->writeElement('dcterms:identifier', $record['identifier']);
+			$writer->startElement('nmo:hasCollection');
+			$writer->writeAttribute('rdf:resource', 'http://nomisma.org/id/rutgers');
+			$writer->endElement();
+			$writer->startElement('nmo:hasTypeSeriesItem');
+			$writer->writeAttribute('rdf:resource', $record['cointype']);
+			$writer->endElement();
+
+			//conditional measurement data
+			if (isset($record['weight'])){
+				$writer->startElement('nmo:hasWeight');
+					$writer->writeAttribute('rdf:datatype', 'http://www.w3.org/2001/XMLSchema#decimal');
+					$writer->text($record['weight']);
+				$writer->endElement();
+			}
+			if (isset($record['diameter'])){
+				$writer->startElement('nmo:hasDiameter');
+					$writer->writeAttribute('rdf:datatype', 'http://www.w3.org/2001/XMLSchema#integer');
+					$writer->text($record['diameter']);
+				$writer->endElement();
+			}
+
+			//conditional images
+			/*if (isset($record['comRef'])){
+				$writer->startElement('foaf:thumbnail');
+				$writer->writeAttribute('rdf:resource', $record['comThumb']);
+				$writer->endElement();
+				$writer->startElement('foaf:depiction');
+				$writer->writeAttribute('rdf:resource', $record['comRef']);
+				$writer->endElement();
+			}*/
+
+			//void:inDataset
+			$writer->startElement('void:inDataset');
+			$writer->writeAttribute('rdf:resource', 'http://coins.libraries.rutgers.edu/romancoins/');
+			$writer->endElement();
+
+			//end nmo:NumismaticObject
+			$writer->endElement();
+		}
+	}
+
+	//end RDF file
+	$writer->endElement();
+	$writer->flush();
+}
+
+function generate_metadata(){
+	//load rights metadata: all rights statements are identical
+	$rightsFile = "https://rucore.libraries.rutgers.edu/api/get/50413/rights/1";
+	$rightsXML = new DOMDocument('1.0', 'UTF-8');
+	if ($rightsXML->load($rightsFile) === FALSE){
+		echo "{$rightsFile} failed to load.\n";
+	} else {
+		foreach ($rightsXML->getElementsByTagNameNS(RUPREFIX, 'rightsDeclaration') as $element) {
+			$rightsStmt = $element->nodeValue;
+		}			
+	}
+	
+	$writer = new XMLWriter();
+	//$writer->openURI("{$collection['project_id']}.void.rdf");
+	$writer->openURI('php://output');
+	$writer->startDocument('1.0','UTF-8');
+	$writer->setIndent(true);
+	//now we need to define our Indent string,which is basically how many blank spaces we want to have for the indent
+	$writer->setIndentString("    ");
+	
+	$writer->startElement('rdf:RDF');
+	$writer->writeAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema#');
+	$writer->writeAttribute('xmlns:nm', "http://nomisma.org/id/");
+	$writer->writeAttribute('xmlns:nmo', "http://nomisma.org/ontology#");
+	$writer->writeAttribute('xmlns:dcterms', "http://purl.org/dc/terms/");
+	$writer->writeAttribute('xmlns:foaf', "http://xmlns.com/foaf/0.1/");
+	$writer->writeAttribute('xmlns:geo', "http://www.w3.org/2003/01/geo/wgs84_pos#");
+	$writer->writeAttribute('xmlns:rdf', "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+	$writer->writeAttribute('xmlns:void', "http://rdfs.org/ns/void#");
+	
+	$writer->startElement('void:Dataset');
+		$writer->writeAttribute('rdf:about', 'http://coins.libraries.rutgers.edu/romancoins/');
+		$writer->writeElement('dcterms:rights', $rightsStmt);
+		
+		
+	//end void:Dataset
+	$writer->endElement();
+	//end RDF file
+	$writer->endElement();
+	$writer->flush();	
+}
+
+?>
