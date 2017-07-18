7bec84134d66ad36414f6a3428a25a38cc64719a
Ethan Gruber <ewg4xuva@gmail.com>
2017-07-18 14:00:56 -0400

    added XSL for restructuring record elements into TEI tables, linked to BM URIs

--- fonts/2.crop.sh
+++ fonts/2.crop.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose *.svg
+for i in `ls *.svg`; do
+inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose $i --verb=FileQuit;
+done
