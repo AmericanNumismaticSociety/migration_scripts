11d30a07468b9aa24074203252148a2071ba20f8
Ethan Gruber <ewg4xuva@gmail.com>
2017-08-23 11:22:03 -0400

    added backup-scripts for backing up eXist-db

---
+++ backup-scripts/scripts/aod.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#First create backup
+PASSWORD=`cat password.txt`
+COLLECTION=aod
+echo "Generating backup."
+cd /usr/local/projects/eXist-db
+java -jar start.jar backup -u admin -p $PASSWORD -b /db/$COLLECTION -d /data/backups/eXist-db/$COLLECTION
+
+#create zip
+echo "Creating .gz file."
+cd /data/backups/eXist-db/$COLLECTION
+NOW=`date +"%Y%m%d%H%M%S"`
+tar czvf $NOW.gz db
+
+#SCP backups to admin
+echo "Uploading to admin server."
+scp -P 4858 $NOW.gz admin.numismatics.org:/usr/local/projects/backups/data/$COLLECTION/$NOW.gz
+
+#delete directory
+
+echo "Cleaning up."
+rm -rf db
+
