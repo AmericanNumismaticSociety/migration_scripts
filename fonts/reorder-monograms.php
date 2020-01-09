15c23308359f815f3dccfca08fd792f44987f63b
Ethan Gruber <ewg4xuva@gmail.com>
2020-01-09 13:32:17 -0500

    added file for logically reordering filenames of monograms

---
+++ fonts/reorder-monograms.php
@@ -0,0 +1,31 @@
+<?php 
+
+/* Date: January 2020
+ * Function: order the monogram SVG list into logical order from the literal order from the filesystem `ls`
+ */
+
+$files = array();
+$ordered = array();
+
+$fh = fopen('files.list','r');
+while ($line = fgets($fh)) {
+    $files[] = str_replace('monogram.houghton.', '', $line);
+}
+fclose($fh);
+
+foreach ($files as $id){
+    preg_match('/^(\d+).*$/', $id, $matches);
+    
+    $ordered[$id] = (int)$matches[1];
+    
+}
+
+asort($ordered);
+//var_dump($ordered);
+
+$file = "new.list";
+foreach ($ordered as $k=>$v){
+    $id = 'monogram.houghton.' . $k;
+    file_put_contents($file, $id, FILE_APPEND);
+}
+?>
