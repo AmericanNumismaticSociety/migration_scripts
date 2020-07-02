71e2ec9a5dfc2bc54385fb4afdde57318897cbad
Ethan Gruber <ewg4xuva@gmail.com>
2020-07-02 14:28:57 -0400

    using mv for all three folders

--- rrdp/rename-files.sh
+++ rrdp/rename-files.sh
@@ -7,6 +7,8 @@ do
 	old=$(echo $line | awk -F';' '{print $1}'|tr '\n' ' ')
 	new=$(echo $line | awk -F';' '{print $2}'|tr '\n' ' ')
 	echo "Moving ${old} => ${new}"
-	cp /data/images/archivesimages/thumbnail/$old ~/$new	
+	mv /data/images/archivesimages/thumbnail/$old /data/images/archivesimages/thumbnail/$new
+	mv /data/images/archivesimages/reference/$old /data/images/archivesimages/reference/$new	
+	mv /data/images/archivesimages/archive/$old /data/images/archivesimages/archive/$new		
 	
 done < $filename
