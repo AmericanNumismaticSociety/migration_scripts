e4bdc311c61d8e532fab446660f7573b539cac1f
Ethan Gruber <ewg4xuva@gmail.com>
2020-07-13 14:39:55 -0400

    updated structure of TEI document

--- rrdp/generate-tei.php
+++ rrdp/generate-tei.php
@@ -19,139 +19,160 @@ generate_tei($object);
 
 /***** FUNCTIONS *****/
 function generate_tei($object){
-    foreach ($object as $id=>$binder){
-        if ($id == 'b01'){
-            $doc = new XMLWriter();
+    foreach ($object as $no=>$binder){
+        echo "Processing {$no}\n";
+        $id = "schaefer.rrdp.{$no}";
+        
+        $doc = new XMLWriter();
+        
+        //$doc->openUri('php://output');
+        $doc->openUri("tei/{$id}.xml");
+        $doc->setIndent(true);
+        //now we need to define our Indent string,which is basically how many blank spaces we want to have for the indent
+        $doc->setIndentString("    ");
+        
+        $doc->startDocument('1.0','UTF-8');
+        
+        //start TEI file
+        $doc->startElement('TEI');
+            $doc->writeAttribute('xmlns', 'http://www.tei-c.org/ns/1.0');
+            $doc->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+            $doc->writeAttribute('xsi:schemaLocation', 'http://www.tei-c.org/ns/1.0 http://www.tei-c.org/release/xml/tei/custom/schema/xsd/tei_all.xsd');
+            $doc->writeAttribute('xml:id', $id);
             
-            $doc->openUri('php://output');
-            //$doc->openUri('tei/' . $id . '.xml');
-            $doc->setIndent(true);
-            //now we need to define our Indent string,which is basically how many blank spaces we want to have for the indent
-            $doc->setIndentString("    ");
-            
-            $doc->startDocument('1.0','UTF-8');
-            
-            //start TEI file
-            $doc->startElement('TEI');
-                $doc->writeAttribute('xmlns', 'http://www.tei-c.org/ns/1.0');
-                $doc->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-                $doc->writeAttribute('xsi:schemaLocation', 'http://www.tei-c.org/ns/1.0 http://www.tei-c.org/release/xml/tei/custom/schema/xsd/tei_all.xsd');
-                $doc->writeAttribute('xml:id', str_replace('b', 'schaefer.binder.', $id));
-                
-                //TEI Header
-                $doc->startElement('teiHeader');
-                    $doc->startElement('fileDesc');
-                        $doc->startElement('titleStmt');
-                            $doc->writeElement('title', 'Schaefer Binder' . ltrim(str_replace('b', '', $id), '0') . ' of Roman Republican Die Study' );
-                            $doc->startElement('author');
-                                $doc->startElement('persName');
-                                    $doc->writeAttribute('ref', 'http://numismatics.org/authority/schaefer_richard');
-                                    $doc->text('Schaefer, Richard');
+            //TEI Header
+            $doc->startElement('teiHeader');
+                $doc->startElement('fileDesc');
+                    $doc->startElement('titleStmt');
+                        $doc->writeElement('title', 'Schaefer Roman Republican Die Study: Binder ' . ltrim(str_replace('b', '', $no), '0'));
+                        $doc->startElement('author');
+                            $doc->startElement('persName');
+                                $doc->writeAttribute('ref', 'http://numismatics.org/authority/schaefer_richard');
+                                $doc->text('Schaefer, Richard, 1946-');
+                            $doc->endElement();
+                        $doc->endElement();
+                    $doc->endElement();
+                    
+                    //these items probably shouldn't have a publisher
+                    $doc->startElement('publicationStmt');
+                        $doc->startElement('publisher');
+                            $doc->writeElement('name', 'American Numismatic Society');
+                            $doc->startElement('idno');
+                                $doc->writeAttribute('type', 'URI');
+                                $doc->text('http://numismatics.org/authority/american_numismatic_society');
+                            $doc->endElement();
+                        $doc->endElement();
+                        $doc->writeElement('pubPlace', 'New York (N.Y.)');
+                    $doc->endElement();
+                    
+                    //sourceDesc
+                    $doc->startElement('sourceDesc');
+                        $doc->startElement('biblStruct');
+                            $doc->startElement('monogr');
+                                $doc->writeElement('title', 'Schaefer Roman Republican Die Study: Binder' . ltrim(str_replace('b', '', $no), '0'));
+                                $doc->startElement('author');
+                                    $doc->startElement('persName');
+                                        $doc->writeAttribute('ref', 'http://numismatics.org/authority/schaefer_richard');
+                                        $doc->text('Schaefer, Richard, 1946-');
+                                    $doc->endElement();
+                                $doc->endElement();
+                                $doc->startElement('imprint');
+                                    $doc->writeElement('date', 'unknown');
                                 $doc->endElement();
                             $doc->endElement();
                         $doc->endElement();
-                        
-                        //these items probably shouldn't have a publisher
-                        $doc->startElement('publicationStmt');
-                            $doc->startElement('publisher');
-                                $doc->writeElement('name', 'American Numismatic Society');
-                                $doc->startElement('idno');
-                                    $doc->writeAttribute('type', 'URI');
-                                    $doc->text('http://numismatics.org/authority/american_numismatic_society');
-                                $doc->endElement();
-                            $doc->endElement();
-                            $doc->writeElement('pubPlace', 'New York (N.Y.)');
-                        $doc->endElement();
-                    //end fileDesc    
-                    $doc->endElement();
-                        
-                    //profileDesc (general document metadata)
-                    $doc->startElement('profileDesc');
-                        $doc->startElement('langUsage');
-                            $doc->startElement('language');
-                                $doc->writeAttribute('ident', 'en');
-                                $doc->text('English');
-                            $doc->endElement();
-                        $doc->endElement();
-                        
-                        $doc->startElement('textClass');
-                            $doc->startElement('classCode');
-                                $doc->writeAttribute('scheme', 'http://vocab.getty.edu/aat/');
-                                $doc->text('300264354');
-                            $doc->endElement();
-                        
-                            //subjects
-                            $doc->startElement('keywords');
-                                $doc->writeAttribute('scheme', 'nmo:TypeSeriesItem');
-                                
-                                foreach ($binder as $page){
-                                    
-                                    //if there are URIs in the 'ref'
-                                    if (count($page['ref']) > 0){
-                                        foreach ($page['ref'] as $uri){
-                                            $type = parse_type($uri);
-                                            if (isset($type)){
-                                                $facs = '#' . str_replace('.jpg', '', $page['filename']);
-                                                
-                                                $doc->startElement('term');
-                                                    $doc->writeAttribute('ref', $type['uri']);
-                                                    $doc->writeAttribute('facs', $facs);
-                                                    $doc->text($type['label']);
-                                                $doc->endElement();
-                                            }      
-                                        }
-                                    }                         
-                                }
-                                
-                            $doc->endElement();
-                        $doc->endElement();
-                    //end profileDesc
+                        //add extent later
                     $doc->endElement();
                     
-                    $doc->startElement('revisionDesc');
-                        $doc->startElement('change');
-                            $doc->writeAttribute('when', substr(date(DATE_W3C), 0, 10));
-                            $doc->text('Generated TEI document by processing list of files generated by ImageMagick identify library.');
+                //end fileDesc    
+                $doc->endElement();
+                    
+                //profileDesc (general document metadata)
+                $doc->startElement('profileDesc');
+                    $doc->startElement('langUsage');
+                        $doc->startElement('language');
+                            $doc->writeAttribute('ident', 'en');
+                            $doc->text('English');
                         $doc->endElement();
                     $doc->endElement();
-                        
                     
-                //end TEI header
+                    $doc->startElement('textClass');
+                        $doc->startElement('classCode');
+                            $doc->writeAttribute('scheme', 'http://vocab.getty.edu/aat/');
+                            $doc->text('300264354');
+                        $doc->endElement();
+                    
+                        //subjects
+                        $doc->startElement('keywords');
+                            $doc->writeAttribute('scheme', 'nmo:TypeSeriesItem');
+                            
+                            foreach ($binder as $page){
+                                
+                                //if there are URIs in the 'ref'
+                                if (count($page['ref']) > 0){
+                                    foreach ($page['ref'] as $uri){
+                                        $type = parse_type($uri);
+                                        if (isset($type)){
+                                            $facs = '#' . str_replace('.jpg', '', $page['filename']);
+                                            
+                                            $doc->startElement('term');
+                                                $doc->writeAttribute('ref', $type['uri']);
+                                                $doc->writeAttribute('facs', $facs);
+                                                $doc->text($type['label']);
+                                            $doc->endElement();
+                                        }      
+                                    }
+                                }                         
+                            }
+                            
+                        $doc->endElement();
+                    $doc->endElement();
+                //end profileDesc
                 $doc->endElement();
                 
-            
-                //facsimiles
-                $num = 0;
-                /*foreach ($binder as $page){
-                    $filename = $page['filename'];
-                    $doc->startElement('facsimile');
-                        $doc->writeAttribute('xml:id', str_replace('.jpg', '', $filename));                    
-                        
-                        //designate first file as depiction
-                        if ($num == 0){
-                            $doc->writeAttribute('style', 'depiction');
-                        }
-                        
-                        $doc->startElement('media');
-                            $doc->writeAttribute('url', "http://images.numismatics.org/archivesimages%2Farchive%2F{$filename}");
-                            $doc->writeAttribute('n', ltrim(str_replace('p', '', $page['page']), '0'));                        
-                            $doc->writeAttribute('mimeType', 'image/jpeg');
-                            $doc->writeAttribute('type', 'IIIFService');
-                            $doc->writeAttribute('height', "{$page['height']}px");
-                            $doc->writeAttribute('width', "{$page['width']}px");
-                        $doc->endElement();
+                $doc->startElement('revisionDesc');
+                    $doc->startElement('change');
+                        $doc->writeAttribute('when', substr(date(DATE_W3C), 0, 10));
+                        $doc->text('Generated TEI document by processing list of files generated by ImageMagick identify library.');
                     $doc->endElement();
+                $doc->endElement();
                     
-                    $num++;
-                }*/
                 
-            //end TEI file
+            //end TEI header
             $doc->endElement();
             
-            //close file
-            $doc->endDocument();
-            $doc->flush();
-        }
+        
+            //facsimiles
+            $num = 0;
+            foreach ($binder as $page){
+                $filename = $page['filename'];
+                $doc->startElement('facsimile');
+                    $doc->writeAttribute('xml:id', str_replace('.jpg', '', $filename));                    
+                    
+                    //designate first file as depiction
+                    if ($num == 0){
+                        $doc->writeAttribute('style', 'depiction');
+                    }
+                    
+                    $doc->startElement('media');
+                        $doc->writeAttribute('url', "http://images.numismatics.org/archivesimages%2Farchive%2F{$filename}");
+                        $doc->writeAttribute('n', ltrim(str_replace('p', '', $page['page']), '0'));                        
+                        $doc->writeAttribute('mimeType', 'image/jpeg');
+                        $doc->writeAttribute('type', 'IIIFService');
+                        $doc->writeAttribute('height', "{$page['height']}px");
+                        $doc->writeAttribute('width', "{$page['width']}px");
+                    $doc->endElement();
+                $doc->endElement();
+                
+                $num++;
+            }
+            
+        //end TEI file
+        $doc->endElement();
+        
+        //close file
+        $doc->endDocument();
+        $doc->flush();
     }
 }
 
