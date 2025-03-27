7a4efaea1a8038634cabe36816042c3ff532070c
Ethan Gruber <ewg4xuva@gmail.com>
2025-03-27 09:51:53 -0400

    switching SITNAM from http to https

--- rrdp/publication-workflow/generate-annotations.php
+++ rrdp/publication-workflow/generate-annotations.php
@@ -9,7 +9,7 @@
 
 ini_set("allow_url_fopen", 1);
 define("DIE_URI_SPACE", "http://numismatics.org/rrdp/id/");
-define("SPECIMEN_URI_SPACE", "http://numismatics.org/sitnam/id/");
+define("SPECIMEN_URI_SPACE", "https://numismatics.org/sitnam/id/");
 
 //an array of sheets for batches of RRC numbers
 $sheets = array('https://docs.google.com/spreadsheets/d/e/2PACX-1vR7jfpBFfSzCLTTXLNCjU0p49GLFUMxbgrb1I5daS0uUjSFrBeM3SjHLUOTYE3NGd7ugMpi29qzu8cn/pub?gid=0&single=true&output=csv',
